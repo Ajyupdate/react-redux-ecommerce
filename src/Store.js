8138cf1c07806cc27c33e0af4f03b6272ea7bf58
Ajibade <ajibadeemmanuel58@gmail.com>
2022-05-14 08:47:57 +0100

    repairing cart

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,6 @@
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
+// import { cartReducer } from "./reducers/CartReducer";
 import {ProductReducer} from "./reducers/ProductReducers";
 
 const initialState = {};
@@ -7,6 +8,7 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     combineReducers({
         products: ProductReducer,
+        // cart: cartReducer,
     }),
     initialState,
     composeEnhancer(applyMiddleware(thunk))
